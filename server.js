eec3d964e5d4bdedc71ab67c8c6f056db51c1817
MitchGianoni <mitchgianoni@yahoo.com>
2018-06-26 15:59:42 -0400

    configured server to run locally and updated code for ESLINT

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+'use strict';
+
 const express = require('express');
 // you'll need to use `queryString` in your `gateKeeper` middleware function
 const queryString = require('query-string');
 
 
 const app = express();
-
+const { PORT } = require('./config');
 // For this challenge, we're hard coding a list of users, because
 // we haven't learned about databases yet. Normally, you'd store
 // user data in a database, and query the database to find
@@ -19,40 +21,40 @@ const app = express();
 // here: https://crackstation.net/hashing-security.htm
 const USERS = [
   {id: 1,
-   firstName: 'Joe',
-   lastName: 'Schmoe',
-   userName: 'joeschmoe@business.com',
-   position: 'Sr. Engineer',
-   isAdmin: true,
-   // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
-   password: 'password'
+    firstName: 'Joe',
+    lastName: 'Schmoe',
+    userName: 'joeschmoe@business.com',
+    position: 'Sr. Engineer',
+    isAdmin: true,
+    // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
+    password: 'password'
   },
   {id: 2,
-   firstName: 'Sally',
-   lastName: 'Student',
-   userName: 'sallystudent@business.com',
-   position: 'Jr. Engineer',
-   isAdmin: true,
-   // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
-   password: 'password'
+    firstName: 'Sally',
+    lastName: 'Student',
+    userName: 'sallystudent@business.com',
+    position: 'Jr. Engineer',
+    isAdmin: true,
+    // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
+    password: 'password'
   },
   {id: 3,
-   firstName: 'Lila',
-   lastName: 'LeMonde',
-   userName: 'lila@business.com',
-   position: 'Growth Hacker',
-   isAdmin: false,
-   // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
-   password: 'password'
+    firstName: 'Lila',
+    lastName: 'LeMonde',
+    userName: 'lila@business.com',
+    position: 'Growth Hacker',
+    isAdmin: false,
+    // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
+    password: 'password'
   },
   {id: 4,
-   firstName: 'Freddy',
-   lastName: 'Fun',
-   userName: 'freddy@business.com',
-   position: 'Community Manager',
-   isAdmin: false,
-   // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
-   password: 'password'
+    firstName: 'Freddy',
+    lastName: 'Fun',
+    userName: 'freddy@business.com',
+    position: 'Community Manager',
+    isAdmin: false,
+    // NEVER EVER EVER store passwords in plain text in real life. NEVER!!!!!!!!!!!
+    password: 'password'
   }
 ];
 
@@ -73,7 +75,7 @@ function gateKeeper(req, res, next) {
 // this endpoint returns a json object representing the user making the request,
 // IF they supply valid user credentials. This endpoint assumes that `gateKeeper` 
 // adds the user object to the request if valid credentials were supplied.
-app.get("/api/users/me", (req, res) => {
+app.get('/api/users/me', (req, res) => {
   // send an error message if no or wrong credentials sent
   if (req.user === undefined) {
     return res.status(403).json({message: 'Must supply valid user credentials'});
@@ -85,6 +87,6 @@ app.get("/api/users/me", (req, res) => {
   return res.json({firstName, lastName, id, userName, position});
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Your app is listening on port ${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(`Your app is listening on port ${PORT}`);
 });
